cc6ec3579a97014f250eee1f4712055668769339
Datseris <datseris.george@gmail.com>
2024-07-08 19:52:13 +0100

    use keyword for pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,5 @@
 cd(@__DIR__)
 using Documenter, MakieThemeing
 pages = ["MakieThemeing" => "index.md"]
-makedocs(pages, modules = [MakieThemeing])
+makedocs(; pages, modules = [MakieThemeing])
 deploydocs(repo = "github.com/Datseris/MakieThemeing.git")
