9b43103366595029b5aa5ea8a8bd5d4f68d0f8a8
Datseris <datseris.george@gmail.com>
2025-03-03 17:16:42 +0000

    clearer docstring for fadelines!

--- src/colormanip.jl
+++ src/colormanip.jl
@@ -52,9 +52,10 @@ function fadecolor(c, N::Int, f = 0.5)
 end
 
 """
-    fadelines!(ax, x, y; kw...)
+    fadelines!(ax, x, y; fade = 0.5, kw...)
 
-Same as [`fadecolor`](@ref) but also call `lines!` with the resulting color.
+Same as [`fadecolor`](@ref) but also call `lines!` with the resulting color
+and the same instructions as `fadecolor`.
 """
 function fadelines!(ax, args...; fade = 0.5, color = :black, kw...)
     c = fadecolor(color, length(args[1]), fade)
